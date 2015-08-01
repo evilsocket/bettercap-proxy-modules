aba8a65018fcfcda563fc509b0baa3d24ab03939
Robbie <txt3rob@gmail.com>
2015-08-01 20:41:54 +0100

    Make images spin
    Makes images spin

---
+++ spinimages.rb
@@ -0,0 +1,31 @@
+=begin
+
+BETTERCAP
+
+Author : Simone 'evilsocket' Margaritelli
+Email  : evilsocket@gmail.com
+Blog   : http://www.evilsocket.net/
+
+This project is released under the GPL 3 license.
+
+=end
+class Spinimages < Proxy::Module
+  def on_request( request, response )
+    # is it a html page?
+    if response.content_type =~ /^text\/html.*/
+      Logger.info "Hacking http://#{request.host}#{request.url} title tag"
+      # make sure to use sub! or gsub! to update the instance
+      response.body.sub!( '</head>', '<style>
+/*
+  Spin all images
+*/ 
+img {
+  /*-webkit-animation: spin 1s linear infinite;
+  animation: spin 1s linear infinite;*/
+}
+
+</style>
+</head>' )
+    end
+  end
+end
