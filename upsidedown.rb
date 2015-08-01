3074afd6ae9d05dd19b6f6a86a38864d31de60a6
Robbie <txt3rob@gmail.com>
2015-08-01 20:37:54 +0100

    upside down
    turns webpages upside down

---
+++ upsidedown.rb
@@ -0,0 +1,30 @@
+=begin
+
+BETTERCAP
+
+Author : Simone 'evilsocket' Margaritelli
+Email  : evilsocket@gmail.com
+Blog   : http://www.evilsocket.net/
+
+This project is released under the GPL 3 license.
+
+=end
+class Upsidedown < Proxy::Module
+  def on_request( request, response )
+    # is it a html page?
+    if response.content_type =~ /^text\/html.*/
+      Logger.info "Hacking http://#{request.host}#{request.url} title tag"
+      # make sure to use sub! or gsub! to update the instance
+      response.body.sub!( '</head>', '<style>
+/* 
+  Turn every website upside down 
+*/ 
+body { 
+  /*-webkit-transform: rotate(180deg);*/ 
+}
+
+</style>
+</head>' )
+    end
+  end
+end
