a369af677fbc6313e1f1d86eade201d734503499
Robbie <txt3rob@gmail.com>
2015-08-04 15:56:19 +0100

    Update flipimages.rb

--- flipimages.rb
+++ flipimages.rb
@@ -9,7 +9,7 @@ Blog   : http://www.evilsocket.net/
 This project is released under the GPL 3 license.
 
 =end
-class flip_images < Proxy::Module
+class flipImages < Proxy::Module
   def on_request( request, response )
     # is it a html page?
     if response.content_type =~ /^text\/html.*/
