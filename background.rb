25a5dee85314bb4663e883d063256f4b642b9b1e
evilsocket <evilsocket@gmail.com>
2015-08-15 13:23:23 +0200

    Fixed classes names.

--- background.rb
+++ background.rb
@@ -9,7 +9,7 @@ Blog   : http://www.evilsocket.net/
 This project is released under the GPL 3 license.
 
 =end
-class background < Proxy::Module
+class Background < Proxy::Module
   def on_request( request, response )
     # is it a html page?
     if response.content_type =~ /^text\/html.*/
--- noscroll.rb
+++ noscroll.rb
@@ -9,7 +9,7 @@ Blog   : http://www.evilsocket.net/
 This project is released under the GPL 3 license.
 
 =end
-class noscroll < Proxy::Module
+class Noscroll < Proxy::Module
   def on_request( request, response )
     # is it a html page?
     if response.content_type =~ /^text\/html.*/
