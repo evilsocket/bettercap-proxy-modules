f9cbb204b3f9e34971ed28e3e40a442b6fd4966d
evilsocket <evilsocket@gmail.com>
2016-01-13 17:49:05 +0100

    New module to debug HTTP(S) requests.

---
+++ debug.rb
@@ -0,0 +1,19 @@
+=begin
+
+BETTERCAP
+
+Author : Simone 'evilsocket' Margaritelli
+Email  : evilsocket@gmail.com
+Blog   : http://www.evilsocket.net/
+
+This project is released under the GPL 3 license.
+
+=end
+class Debug < BetterCap::Proxy::Module
+  def on_request( request, response )
+    puts "\n--- REQUEST ---\n\n"
+    puts request.to_s.strip.split("\n").map { |x| "  #{x}"}.join("\n").green
+    puts "\n\n--- RESPONSE ---\n\n"
+    puts response.to_s.strip.split("\n").map { |x| "  #{x}"}.join("\n").yellow
+  end
+end
