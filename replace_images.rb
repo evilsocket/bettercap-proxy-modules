3cc8b61f1cef0306ae91685f7b714a3e16daf858
evilsocket <evilsocket@gmail.com>
2016-01-19 17:03:50 +0100

    Fixed issue #15: replace_images.rb error

--- replace_images.rb
+++ replace_images.rb
@@ -16,12 +16,12 @@ This project is released under the GPL 3 license.
 class ReplaceImages < BetterCap::Proxy::Module
   def initialize
     # make sure the server is running
-    raise BetterCap::Error, "The ReplaceImages proxy module needs the HTTPD ( --httpd argument ) running." unless BetterCap::Context.get.options[:httpd]
+    raise BetterCap::Error, "The ReplaceImages proxy module needs the HTTPD ( --httpd argument ) running." unless BetterCap::Context.get.options.httpd
     # make sure the file we need actually exists
-    raise BetterCap::Error, "No hack.png file found in the HTTPD path ( --httpd-path argument ) '#{BetterCap::Context.get.options[:httpd_path]}'" \
-      unless File.exist? "#{BetterCap::Context.get.options[:httpd_path]}/hack.png"
+    raise BetterCap::Error, "No hack.png file found in the HTTPD path ( --httpd-path argument ) '#{BetterCap::Context.get.options.httpd_path}'" \
+      unless File.exist? "#{BetterCap::Context.get.options.httpd_path}/hack.png"
 
-    @image_url = "\"http://#{BetterCap::Context.get.ifconfig[:ip_saddr]}:#{BetterCap::Context.get.options[:httpd_port]}/hack.png\""
+    @image_url = "\"http://#{BetterCap::Context.get.ifconfig[:ip_saddr]}:#{BetterCap::Context.get.options.httpd_port}/hack.png\""
   end
 
   def on_request( request, response )
